75ffa92732b28697be9989bea0dc25a23ad29c6f
silasnordgren <nordgrensilas@gmail.com>
2017-10-30 13:17:19 +0100

    Added Unirest for testing Jetty server

--- src/test/java/org/hydrogen/jetty/JettyServerTest.java
+++ src/test/java/org/hydrogen/jetty/JettyServerTest.java
@@ -1,13 +1,33 @@
 package org.hydrogen.jetty;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.hydrogen.HTMLResponse;
 import org.hydrogen.Server;
+import org.hydrogen.TextResponse;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class JettyServerTest {
+    private final int port = 8080;
+    private final String localhost = "http://localhost:" + port;
+
     @Test
-    public void testHTML() {
-        Server server = JettyServer.start(request -> HTMLResponse.ok("<h1>Hi!</h1>"));
-        server.join();
+    public void testHTML() throws UnirestException {
+        final String expected = "<h1>Hi!</h1>";
+        Server server = JettyServer.start(request -> HTMLResponse.ok(expected), port);
+        String result = Unirest.get(localhost).asString().getBody();
+        assertEquals(expected, result);
+        server.stop();
+    }
+
+    @Test
+    public void testString() throws UnirestException {
+        final String expected = "Hello, world!";
+        Server server = JettyServer.start(request -> TextResponse.ok(expected), port);
+        String result = Unirest.get(localhost).asString().getBody();
+        assertEquals(expected, result);
+        server.stop();
     }
 }
