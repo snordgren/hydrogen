00837023cb4a0acd24ba6bc3a0929eb72566f36c
silasnordgren <nordgrensilas@gmail.com>
2017-11-12 20:34:10 +0100

    Added Javadoc

--- core/src/main/java/org/hydrogen/Filter.java
+++ core/src/main/java/org/hydrogen/Filter.java
@@ -1,6 +1,17 @@
 package org.hydrogen;
 
+/**
+ * A filter is a function transforming one handler into another.
+ */
 @FunctionalInterface
 public interface Filter {
+
+    /**
+     * Applies this filter to a handler, creating a new handler that works
+     * differently.
+     *
+     * @param handler The handler that is to be transformed.
+     * @return The transformed handler, with different or additional behavior.
+     */
     Handler apply(Handler handler);
 }
